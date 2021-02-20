f4c853f81aceed981f3ace499f117ad00bd5e733
ravenclaw900 <50060110+ravenclaw900@users.noreply.github.com>
2021-02-19 18:16:35 -0600

    Add menu bar

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,27 @@
 <template>
-<div></div>
+  <ActionBar />
+  <Main />
 </template>
+
+<script>
+import ActionBar from "@/components/ActionBar";
+import Main from "@/components/Main";
+
+export default {
+  components: {
+    ActionBar,
+    Main,
+  },
+};
+</script>
+
+<style>
+html,
+body,
+#app {
+  height: 100%;
+  margin: 0;
+  overflow: hidden;
+  font-family: sans-serif;
+}
+</style>
